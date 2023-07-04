cb567914c9db8dfafd52152b93f203b5b08cc955
Aleksey <ovsyannikov.09@yandex.ru>
2023-07-04 22:40:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задание №1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Console.Write("Напишите трехзначное число: ");
+// string stringNumber = Console.ReadLine();
+// Console.WriteLine(stringNumber[1]);
+// Задание №2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. До 10 символов
+// Console.Write("Введите число: ");
+// int Value = Convert.ToInt32(Console.ReadLine());
+// int Length = Value.ToString().Length;
+// if (Length >= 3) 
+// {
+// 	while (Value > 999)
+// 	{
+// 		Value = Value / 10;
+// 	}
+// 	int result = Value % 10;
+// 	Console.WriteLine($"Третья цифра {result}");
+// } else
+// {
+// 	Console.WriteLine("Третьей цифры нет");
+// }
+// Задание №3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// {
+
+//     if (number <= 5)
+//     {
+//         Console.WriteLine("Это будний день");
+//     }
+//     if (number > 5 && number <8)
+//     {
+//         Console.WriteLine("Ура, выходной");
+//     }
+//     if (number >= 8)
+//     {
+//         Console.WriteLine("Далеко пошел");
+//     }
+// }
